b4fcc138d8e91f87297a87a225c709f7477bf17e
jiaxiaoyu <xyyy1420@gmail.com>
2024-06-12 10:48:50 +0800

    rewrite build bbl script, using config file replace command args
    Signed-off-by: jiaxiaoyu <xyyy1420@gmail.com>

--- checkpoint_scripts/generate_bbl.py
+++ checkpoint_scripts/generate_bbl.py
@@ -1,118 +1,102 @@
 import os
 import shutil
 import subprocess
+import os
+import subprocess
+import shutil
 
+class Builder:
+    def __init__(self, env_vars_to_check=None):
+        """Initialize Builder with optional environment variables to check."""
+        self.env_vars = {}
+        if env_vars_to_check:
+            for env_var in env_vars_to_check:
+                self.env_vars[env_var] = self.check_directory(env_var)
 
-def build_spec_bbl(spec, build_log_folder, spec_bin_folder, bin_suffix, assembly_folder):
+    @staticmethod
+    def check_directory(env_var):
+        """Check if the environment variable is set and points to an existing directory."""
+        path = os.environ.get(env_var)
+        if not path:
+            raise EnvironmentError(f"Environment variable {env_var} is not set.")
+        if not os.path.exists(path):
+            raise EnvironmentError(f"Path {path} for environment variable {env_var} does not exist.")
+        return path
 
-    print(f"build {spec}-bbl-linux-spec...")
+    @staticmethod
+    def run_commands(commands, cwd, out_log, err_log):
+        """Run a series of commands in a specified directory, logging output and errors."""
+        with open(out_log, "a", encoding="utf-8") as out, open(err_log, "a", encoding="utf-8") as err:
+            for command in commands:
+                res = subprocess.run(command, cwd=cwd, stdout=out, stderr=err, check=True)
+                res.check_returncode()
 
-    with open(
-        os.path.join(build_log_folder, f"build-{spec}-out.log"), "w", encoding="utf-8"
-    ) as out, open(
-        os.path.join(build_log_folder, f"build-{spec}-err.log"), "w", encoding="utf-8"
-    ) as err:
-        pk_folder=os.environ.get("RISCV_PK_HOME")
-        if pk_folder is None or not os.path.exists(pk_folder):
-            print("riscv-pk is not exists")
-            exit(-1)
+    def build_spec_bbl(self, spec, build_log_folder, spec_bin_folder, bin_suffix, assembly_folder):
+        print("build_spec_bbl")
+        PK_HOME = self.env_vars.get("RISCV_PK_HOME")
+        if not PK_HOME:
+            raise EnvironmentError("Environment variable RISCV_PK_HOME is not set or not initialized.")
 
-        res = subprocess.run(
-            ["make", "clean"], cwd=pk_folder, stdout=out, stderr=err, check=False
-        )
-        res.check_returncode()
-        res = subprocess.run(
-            ["make", "-j70"], cwd=pk_folder, stdout=out, stderr=err, check=False
-        )
-        res.check_returncode()
+        print(f"build {spec}-bbl-linux-spec...")
 
-    bbl_bin_src_path = os.path.join(pk_folder, "build", "bbl.bin")
-    bbl_bin_dest_path = os.path.join(
-        spec_bin_folder,
-        "{}{}".format(spec, bin_suffix)
-    )
-    shutil.copy(bbl_bin_src_path, bbl_bin_dest_path)
+        out_log = os.path.join(build_log_folder, f"build-{spec}-out.log")
+        err_log = os.path.join(build_log_folder, f"build-{spec}-err.log")
 
-    bbl_txt_src_path = os.path.join(pk_folder, "build", "bbl.txt")
-    bbl_txt_dest_path = os.path.join(
-        assembly_folder,
-        "{}{}{}".format(spec,
-                        bin_suffix, ".pk.s")
-    )
-    shutil.copy(bbl_txt_src_path, bbl_txt_dest_path)
+        pk_commands = [["make", "clean"], ["make", "-j70"]]
+        self.run_commands(pk_commands, PK_HOME, out_log, err_log)
 
-    kernel_txt_src_path = os.path.join(
-        pk_folder, "build", "vmlinux.txt"
-    )
-    kernel_txt_dest_path = os.path.join(
-        assembly_folder,
-        "{}{}{}".format(spec,
-                        bin_suffix, ".vmlinux.s")
-    )
-    shutil.copy(kernel_txt_src_path, kernel_txt_dest_path)
+        self.copy_files(PK_HOME, spec, bin_suffix, assembly_folder, spec_bin_folder, {
+            "build/bbl.bin": "",
+            "build/bbl.txt": ".pk.s",
+            "build/vmlinux.txt": ".vmlinux.s"
+        })
 
+    def build_opensbi_payload(self, spec, build_log_folder, spec_bin_folder, bin_suffix, gcpt_bin_folder, gcpt_bin_suffix, assembly_folder, withGCPT=False):
+        required_vars = ["LINUX_HOME", "OPENSBI_HOME", "XIANGSHAN_FDT", "NEMU_HOME"]
+        if not all([self.env_vars.get(var) for var in required_vars]):
+            raise EnvironmentError("One or more required environment variables are not set or not initialized.")
 
-def build_bbl_as_gcpt_payload(spec, build_log_folder, spec_bin_folder, bin_suffix, gcpt_bin_folder, gcpt_bin_suffix, assembly_folder):
-    with open(
-        os.path.join(
-            build_log_folder, f"build-{spec}-aspayload-out.log"
-        ), "w", encoding="utf-8"
-    ) as out, open(
-        os.path.join(
-            build_log_folder, f"build-{spec}-aspayload-err.log"
-        ), "w", encoding="utf-8"
-    ) as err:
+        print(f"build {spec}-opensbi-linux-spec...{' with GCPT' if withGCPT else ''}")
 
-        res = subprocess.run(
-            ["make", "clean"],
-            cwd=os.path.join(
-                os.environ.get("NEMU_HOME"), "resource", "gcpt_restore"
-            ),
-            stdout=out,
-            stderr=err,
-            check=False
-        )
-        res.check_returncode()
+        out_log = os.path.join(build_log_folder, f"build-binary-with-{spec}-out.log")
+        err_log = os.path.join(build_log_folder, f"build-binary-with-{spec}-err.log")
 
-        res = subprocess.run(
-            [
-                "make", "GCPT_PAYLOAD_PATH={}".format(
-                    os.path.abspath(
-                        os.path.join(
-                            spec_bin_folder,
-                            "{}{}".format(spec,
-                                          bin_suffix)
-                        )
-                    )
-                )
-            ],
-            cwd=os.path.join(
-                os.environ.get("NEMU_HOME"), "resource", "gcpt_restore"
-            ),
-            stdout=out,
-            stderr=err,
-            check=False
-        )
-        res.check_returncode()
+        linux_commands = [["make", "clean"], ["make", "-j10"]]
+        opensbi_commands = [
+            ["rm", "-rf", "build"],
+            ["make", "PLATFORM=generic", f"FW_PAYLOAD_PATH={self.env_vars['LINUX_HOME']}/arch/riscv/boot/Image", f"FW_FDT_PATH={self.env_vars['XIANGSHAN_FDT']}", "FW_PAYLOAD_OFFSET=0x160000", "-j10"]
+        ]
 
-    gcpt_bin_src_path = os.path.join(
-        os.environ.get("NEMU_HOME"), "resource", "gcpt_restore", "build",
-        "gcpt.bin"
-    )
-    gcpt_bin_dest_path = os.path.join(
-        gcpt_bin_folder,
-        "{}{}".format(spec,
-                      gcpt_bin_suffix)
-    )
-    shutil.copy(gcpt_bin_src_path, gcpt_bin_dest_path)
+        self.run_commands(linux_commands, self.env_vars["LINUX_HOME"], out_log, err_log)
+        self.run_commands(opensbi_commands, self.env_vars["OPENSBI_HOME"], out_log, err_log)
 
-    gcpt_txt_src_path = os.path.join(
-        os.environ.get("NEMU_HOME"), "resource", "gcpt_restore", "build",
-        "gcpt.txt"
-    )
-    gcpt_txt_dest_path = os.path.join(
-        assembly_folder,
-        "{}{}{}".format(spec,
-                        gcpt_bin_suffix, ".gcpt.s")
-    )
-    shutil.copy(gcpt_txt_src_path, gcpt_txt_dest_path)
+        fw_payload_bin_dst_path = os.path.join(spec_bin_folder, f"{spec}{bin_suffix}")
+        self.copy_files(self.env_vars["OPENSBI_HOME"], spec, bin_suffix, assembly_folder, spec_bin_folder, {
+            "build/platform/generic/firmware/fw_payload.bin": ""
+        })
+
+        if withGCPT:
+            gcpt_commands = [["make", "clean"], ["make", f"GCPT_PAYLOAD_PATH={fw_payload_bin_dst_path}"]]
+            self.run_commands(gcpt_commands, os.path.join(self.env_vars["NEMU_HOME"], "resource", "gcpt_restore"), out_log, err_log)
+            self.copy_files(os.path.join(self.env_vars["NEMU_HOME"], "resource", "gcpt_restore"), spec, gcpt_bin_suffix, assembly_folder, gcpt_bin_folder, {
+                "build/gcpt.bin": "",
+                "build/gcpt.txt": ".gcpt.s"
+            })
+
+    @staticmethod
+    def copy_files(base_path, spec, suffix, assembly_folder, bin_folder, files):
+        """Copy files from base path to their respective destinations with appropriate suffixes."""
+        for src, dest_suffix in files.items():
+            src_path = os.path.join(base_path, src)
+            bin_dest_path = os.path.join(bin_folder, f"{spec}{suffix}")
+            shutil.copy(src_path, bin_dest_path)
+
+            if dest_suffix:
+                assembly_dest_path = os.path.join(assembly_folder, f"{spec}{suffix}{dest_suffix}")
+                shutil.copy(src_path, assembly_dest_path)
+
+# Example usage
+#builder = Builder(env_vars_to_check=["LINUX_HOME", "OPENSBI_HOME", "XIANGSHAN_FDT", "NEMU_HOME", "RISCV_PK_HOME"])
+#builder.build_spec_bbl("example_spec", "/path/to/logs", "/path/to/bin", "_suffix", "/path/to/assembly")
+#builder.build_opensbi_payload("example_spec", "/path/to/logs", "/path/to/bin", "_suffix", "/path/to/gcpt_bin", "_gcpt_suffix", "/path/to/assembly", withGCPT=True)
+#
