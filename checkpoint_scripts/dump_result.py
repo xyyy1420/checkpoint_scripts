5c09341bc93b2d2225eb3f21bbb729cfae6ed713
jiaxiaoyu <xyyy1420@gmail.com>
2024-06-18 14:52:01 +0800

    rewirte dump result, support using times, ids point to generate checkpoint folder
    Signed-off-by: jiaxiaoyu <xyyy1420@gmail.com>

--- checkpoint_scripts/dump_result.py
+++ checkpoint_scripts/dump_result.py
@@ -2,6 +2,7 @@ import os
 import re
 import json
 from pathlib import Path
+from itertools import product
 
 app_list = [
     "bwaves", "gamess_cytosine", "gamess_gradient", "gamess_triazolium", "milc",
@@ -19,25 +20,40 @@ spec_2017_list = [
     "deepsjeng", "leela", "exchange2", "xz_cld", "xz_combined", "xz_cpu2006"
 ]
 
-def profiling_instrs(profiling_log, spec_app):
+spec2017_int_list = [
+    "perlbench_diff", "perlbench_spam", "perlbench_split", "gcc_pp_O2",
+    "gcc_pp_O3", "gcc_ref32_O3", "gcc_ref32_O5", "gcc_small_O3", "mcf",
+    "omnetpp", "xalancbmk", "x264_pass1", "x264_pass2", "x264_seek",
+    "deepsjeng", "leela", "exchange2", "xz_cld", "xz_combined", "xz_cpu2006"
+]
+
+spec2017_fp_list=list(set(spec_2017_list)-set(spec2017_int_list))
+
+def profiling_instrs(profiling_log, spec_app, using_new_script=True):
     regex = r".*total guest instructions = (.*)\x1b.*"
-    with open(os.path.join(profiling_log, spec_app,"profiling.out.log"),
-              "r") as f:
+    new_path = os.path.join(profiling_log, spec_app, "profiling.out.log")
+    old_path = os.path.join(profiling_log, "{}-out.log".format(spec_app))
+
+    if using_new_script:
+        path=new_path
+    else:
+        path=old_path
+
+    with open(path, "r", encoding="utf-8") as f:
         for i in f.readlines():
             if "total guest instructions" in i:
                 match = re.findall(regex, i)
                 match = match[0].replace(',', '')
                 return match
-        else:
-            return 0
+        return 0
 
 
 def cluster_weight(cluster_path, spec_app):
     points = {}
     weights = {}
 
-    weights_path = "{}/{}/weights0".format(cluster_path, spec_app)
-    simpoints_path = "{}/{}/simpoints0".format(cluster_path, spec_app)
+    weights_path = f"{cluster_path}/{spec_app}/weights0"
+    simpoints_path = f"{cluster_path}/{spec_app}/simpoints0"
 
     with open(weights_path, "r") as f:
         for line in f.readlines():
@@ -88,34 +104,35 @@ def per_checkpoint_generate_worklist(cpt_path, target_path):
             name = path.replace('/', "_", 1)
             print("{} {} 0 0 20 20".format(name, path), file=f)
 
-result = {
-    "cl_res":
-        os.path.join(
-            "/path/to/cluster-0-0"
-        ),
-    "profiling_log":
-        os.path.join(
-            "/path/to/logs/profiling-0"
-        ),
-    "checkpoint_path":
-        os.path.join(
-            "/path/to/checkpoint-0-0-0"
-        ),
-    "json_path":
-        os.path.join(
-            "/path/to/checkpoint-0-0-0/cluster-0-0.json"
-        ),
-    "list_path":
-        os.path.join(
-            "/path/to/checkpoint-0-0-0/checkpoint.lst"
-        )
-}
-#
-#def generate_result(buffer, log_folder):
-#    buffer_path = Path(buffer)
-#    log_path = Path(log_folder)
-#    profiling_logs = list(log_path.glob("profiling-*"))
-#
 
-per_checkpoint_generate_json(result["profiling_log"], result["cl_res"], app_list, result["json_path"])
-per_checkpoint_generate_worklist(result["checkpoint_path"], result["list_path"])
+def generate_result_list(base_path, times, ids):
+    result_list = []
+
+    for i, j, k in product(range(ids[0], times[0]), range(ids[1], times[1]), range(ids[2], times[2])):
+        cluster = f"cluster-{i}-{j}"
+        profiling = f"profiling-{k}"
+        checkpoint = f"checkpoint-{i}-{j}-{k}"
+        result_list.append({
+            "cl_res": os.path.join(base_path, cluster),
+            "profiling_log": os.path.join(base_path, "logs", profiling),
+            "checkpoint_path": os.path.join(base_path, checkpoint),
+            "json_path": os.path.join(base_path, checkpoint, f"{cluster}.json"),
+            "list_path": os.path.join(base_path, checkpoint, "checkpoint.lst"),
+        })
+        
+    return result_list
+
+
+
+def dump_result(base_path, spec_app_list, times, ids):
+    result_list = generate_result_list(base_path, times, ids)
+
+    for result in result_list:
+        per_checkpoint_generate_json(result["profiling_log"], result["cl_res"], spec_app_list, result["json_path"])
+        per_checkpoint_generate_worklist(result["checkpoint_path"], result["list_path"])
+
+base_path = "/path/to/archive"
+times = [1, 1, 1]
+ids = [0, 0, 0]
+
+dump_result(base_path, spec_2017_list, times, ids)
