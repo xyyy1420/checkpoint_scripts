51cf5d50fb6fd5269623ba4faa68197aef0d540c
Zhou Yaoyang <zhouyaoyang17@mails.ucas.edu.cn>
2025-01-07 11:32:48 +0800

    also record checkpoints that are not used in a specific version
    - We can optionally add results of these unused checkpoints in the future

--- checkpoint_scripts/select_points.py
+++ checkpoint_scripts/select_points.py
@@ -32,7 +32,9 @@ with open(weight_file) as f:
     js = json.load(f)
 
 new_js = {}
+rest_js = {}
 files = []
+rest_files = []
 
 workloads = []
 if len(test_set):
@@ -48,22 +50,30 @@ for workload, info in js.items():
 
     if workload not in new_js:
         new_js[workload] = {}
+        rest_js[workload] = {}
     new_js[workload]['insts'] = js[workload]['insts']
     new_js[workload]['points'] = {}
+    rest_js[workload]['insts'] = js[workload]['insts']
+    rest_js[workload]['points'] = {}
     # sorted_points = sorted(info['points'], reverse=True)
     # sort info['points'] by value
     sorted_points = sorted(info['points'], key=lambda x: info['points'][x], reverse=True)
+    cover_satisfied = False
     for point in sorted_points:
         weight = info['points'][point]
 
         cumm_weight += float(weight)
         count += 1
         print(workload, point, weight)
-        files.append(f'{workload}_{point} {workload}_{point}_{weight}/0/')
-        new_js[workload]['points'][point] = weight
 
-        if count >= count_limit or cumm_weight >= coverage_limit:
-            break
+        if cover_satisfied:
+            rest_files.append(f'{workload}_{point} {workload}_{point}_{weight}/0/')
+            rest_js[workload]['points'][point] = weight
+        else:
+            files.append(f'{workload}_{point} {workload}_{point}_{weight}/0/')
+            new_js[workload]['points'][point] = weight
+            if count >= count_limit or cumm_weight >= coverage_limit:
+                cover_satisfied = True
 
 n_chunks = 1
 chunk_size = len(files) // n_chunks
@@ -74,5 +84,12 @@ for i in range(n_chunks):
         outf.write('\n'.join(chunk))
     files = files[chunk_size:]
 
+with open(f'{args.output_name}_{test_set}_cover{coverage_limit:.2f}{count_limit_str}-unused.lst.{i}', 'w') \
+        as outf:
+    outf.write('\n'.join(rest_files))
+
 with open(f'{args.output_name}_{test_set}_cover{coverage_limit:.2f}{count_limit_str}.json', 'w') as outf:
     json.dump(new_js, outf, indent=4)
+
+with open(f'{args.output_name}_{test_set}_cover{coverage_limit:.2f}{count_limit_str}-unused.json', 'w') as outf:
+    json.dump(rest_js, outf, indent=4)
